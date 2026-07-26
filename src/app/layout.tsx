c7cf9af6137c397dd04b8a80815e8d0d1e5c4c2d
Sudarsan Kumar <sudharsan58585@gmail.com>
2026-07-26 17:32:22 +0530

    Ligth theme correction

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" className="dark" suppressHydrationWarning>
       <body>
         <Script 
           src="https://sdk.cashfree.com/js/v3/cashfree.js"
